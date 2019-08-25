0196858d1c60a2e7a66c08a86557398dd618b1b7
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-25 16:37:12 -0600

    Add EdamamApiService
    Co-authored-by: Ryan Miller <42855291+ryanmillergm@users.noreply.github.com>

---
+++ util/EdamamApiService.js
@@ -0,0 +1,29 @@
+var rp = require('request-promise');
+
+class EdamamApiService {
+  constructor(foodType) {
+    this.foodType = foodType;
+  }
+
+  recipeResults() {
+    let domain = 'https://api.edamam.com'
+    let options = {
+      uri: domain + '/search',
+      qs: {
+        q: this.foodType,
+        app_id: process.env.EDAMAM_ID,
+        app_key: process.env.EDAMAM_KEY,
+        from: 0,
+        to: 9,
+        health: 'alcohol-free'
+      },
+      json: true
+    };
+
+    return rp(options);
+  }
+}
+
+module.exports = {
+  EdamamApiService: EdamamApiService
+}
