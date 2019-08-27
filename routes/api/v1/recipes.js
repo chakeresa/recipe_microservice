8e13e0a72c566f7b62bb5062b1cfb20694c14fb5
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-27 14:57:33 -0600

    Attempt food_search endpoint
    Co-authored-by: Ryan Miller <42855291+ryanmillergm@users.noreply.github.com>

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET recipes based on food type. */
 router.get('/food_search', function(req, res, next) {
-  res.status(200).send({ message: 'hello' });
+  console.log(`q param = ${req.query.q}`)
+  FoodType.findOne({
+    where: {
+      name: req.query.q
+    }
+  }).then(foodTypeResource => {
+    if (foodTypeResource) {
+      console.log(`foodTypeResource = ${foodTypeResource}`)
+      let recipes = foodTypeResource.recipes
+      res.status(200).send(JSON.stringify(recipes));
+    } else {
+      res.status(200).send(JSON.stringify([]));
+    }
+  }).catch(err => {
+    let response = {error: err};
+    res.status(500).send(JSON.stringify(response));
+  })
 });
 
 module.exports = router;
