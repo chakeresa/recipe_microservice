a59c86220ee3800c7ef6c93ca00ba1c2ef845878
Ryan Miller <ryanmillergm@gmail.com>
2019-08-27 17:24:24 -0600

    Fixed recipe.js with model request and and include recipe and ingredients to FoodType

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var FoodType = require("../../../models").FoodType;
+var Ingredient = require("../../../models").Ingredient;
+var Recipe = require("../../../models").Recipe;
 
 /* GET recipes based on food type. */
 router.get('/food_search', function(req, res, next) {
-  console.log(`q param = ${req.query.q}`)
+  console.log("=-=-=-=-=-=-=-=-")
+  console.log(`q query = ${req.query.q}`)
   FoodType.findOne({
-    where: {
-      name: req.query.q
-    }
+    where: { name: req.query.q },
+    include: [{
+      model: Recipe,
+      as: "recipes",
+      include: [{
+        model: Ingredient,
+        as: "ingredients"
+      }]
+    }]
   }).then(foodTypeResource => {
     if (foodTypeResource) {
-      console.log(`foodTypeResource = ${foodTypeResource}`)
-      let recipes = foodTypeResource.recipes
+      let recipes = foodTypeResource.recipes[0].dataValues
+      console.log("recipes =")
+      console.log(recipes)
+      console.log("--------============------------------>>>>")
+
+      res.setHeader("Content-Type", "application/json");
       res.status(200).send(JSON.stringify(recipes));
     } else {
       res.status(200).send(JSON.stringify([]));
     }
   }).catch(err => {
+    console.log(err)
     let response = {error: err};
     res.status(500).send(JSON.stringify(response));
   })
--- test/requests/recipes/food_search.test.js
+++ test/requests/recipes/food_search.test.js
@@ -9,74 +9,55 @@ describe('api/v1/recipes/food_search GET', function () {
   describe('user can get all recipes from the database for a particular foodtype', function () {
     it('returns JSON with the attributes and ingredients', (done) => {
       let foodTypeName = 'pizza';
-      FoodType.create({
+
+      return FoodType.create({
+        id: 1,
         name: 'chicken',
-        recipes: [
-          {
-            name: 'chicken parmesan',
-            calories: 400,
-            timeToPrepare: 95,
-            servings: 2,
-            ingredients: [
-              { text: '2 chicken breasts' },
-              { text: '1 cup parmesan cheese' }
-            ]
-          },
-          {
-            name: 'chicken noodle soup',
-            calories: 300,
-            timeToPrepare: 80,
-            servings: 8,
-            ingredients: [
-              { text: '5 cups chicken stock' },
-              { text: '1/2 lb noodles' }
-            ]
-          }
-        ]
-      }).then(foodType1 => {
-        return FoodType.create({
-          name: foodTypeName,
-          recipes: [
-            {
-              name: 'margarita pizza',
-              calories: 500,
-              timeToPrepare: 120,
-              servings: 6,
-              ingredients: [
-                { text: '1 lb pizza dough' },
-                { text: '1 cup sliced tomatoes' },
-                { text: '1 cup cheese' }
-              ]
-            },
-            {
-              name: 'pepperoni pizza',
-              calories: 600,
-              timeToPrepare: 90,
-              servings: 4,
-              ingredients: [
-                { text: '2 cups pizza dough' },
-                { text: '1.5 cups tomato sauce' },
-                { text: '1/2 cup pepperonis' }
-              ]
-            }
-          ]
+        recipes: []
+      }).then(foodType => {
+
+        return Recipe.create({
+          id: 1,
+          name: 'chicken parmesan',
+          calories: 400,
+          timeToPrepare: 95,
+          servings: 2,
+          ingredients: [],
+          FoodTypeId: 1
         })
-      }).then(foodType2 => {
-        return request(app)
-          .get(`/api/v1/recipes/food_search?q=${foodTypeName}`)
+      }).then(recipe => {
+        let recipeChickenParm = recipe
+
+        return Ingredient.create({
+          id: 1,
+          text: '2 chicken breasts' ,
+          text: '1 cup parmesan cheese',
+          RecipeId: 1
+          })
+        }).then(() => {
+          return FoodType.findAll({
+            where: { name: "chicken" }
+        })
+        }).then(foodType => {
+          console.log("---------------------------")
+          console.log(foodType[0].dataValues)
+
+          return request(app)
+          .get(`/api/v1/recipes/food_search?q=chicken`)
       }).then(response => {
         expect(response.statusCode).to.equal(200);
-
+        console.log("response========")
+        console.log(response.body)
         expect(response.body).to.have.lengthOf(2);
 
         let firstRecipe = response.body[0];
         expect(firstRecipe).to.include.all.keys('id', 'name', 'calories', 'timeToPrepare', 'servings', 'ingredients');
         expect(firstRecipe).to.not.include.key('createdAt');
         expect(firstRecipe).to.not.include.key('updatedAt');
-        
+
         expect(firstRecipe.calories).to.equal(500);
         expect(firstRecipe.name).to.equal('margarita pizza');
-        
+
         let firstIngredient = firstIngredient.ingredients[0];
         expect(firstIngredient).to.include.all.keys('id', 'text');
         expect(firstIngredient).to.not.include.key('createdAt');
