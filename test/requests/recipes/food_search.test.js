30f5d35e4d75218b49d2d88f0c1e614351cd635a
Ryan Miller <ryanmillergm@gmail.com>
2019-08-28 09:54:41 -0600

    Refactored test

--- test/requests/recipes/food_search.test.js
+++ test/requests/recipes/food_search.test.js
@@ -10,40 +10,68 @@ describe('api/v1/recipes/food_search GET', function () {
     it('returns JSON with the attributes and ingredients', (done) => {
       let foodTypeName = 'pizza';
 
-       FoodType.create({
-        id: 1,
-        name: 'chicken',
-        recipes: []
-      }).then(foodType => {
-
-        return Recipe.create({
+      FoodType.bulkCreate([
+        {
           id: 1,
-          name: 'chicken parmesan',
-          calories: 400,
-          timeToPrepare: 95,
-          servings: 2,
-          ingredients: [],
-          FoodTypeId: 1
-        })
+          name: 'chicken'
+        },
+        {
+          id: 2,
+          name: 'pizza'
+        }
+      ]).then(foodType => {
+
+        return Recipe.bulkCreate([
+          {
+            id: 1,
+            name: 'chicken parmesan',
+            calories: 400,
+            timeToPrepare: 95,
+            servings: 2,
+            FoodTypeId: 1
+          },
+          {
+            id: 2,
+            name: 'pepperoni pizza',
+            calories: 400,
+            timeToPrepare: 95,
+            servings: 2,
+            FoodTypeId: 2
+          },
+          {
+            id: 3,
+            name: 'chicken noodle soup',
+            calories: 600,
+            timeToPrepare: 180,
+            servings: 4,
+            FoodTypeId: 1
+          }
+        ])
       }).then(recipe => {
 
-        return Ingredient.create({
-          id: 1,
-          text: '2 chicken breasts' ,
-          text: '1 cup parmesan cheese',
-          RecipeId: 1
-          })
-        }).then(() => {
-          return FoodType.findAll({
-            where: { name: "chicken" }
-        })
-        }).then(foodType => {
-
-          return request(app)
-          .get(`/api/v1/recipes/food_search?q=chicken`)
+        return Ingredient.bulkCreate([
+          {
+            id: 1,
+            text: '2 chicken breasts' ,
+            RecipeId: 1
+          },
+          {
+            id: 2,
+            text: '1 cup grated parmesan' ,
+            RecipeId: 1
+          },
+          {
+            id: 3,
+            text: '1 whole chicken' ,
+            RecipeId: 2
+          }
+        ])
+      }).then(ingredient => {
+        return request(app)
+        .get(`/api/v1/recipes/food_search?q=chicken`)
       }).then(response => {
         expect(response.statusCode).to.equal(200);
-        expect(response.body).to.have.lengthOf(1);
+        expect(response.body).to.have.lengthOf(2);
 
         let firstRecipe = response.body[0];
         expect(firstRecipe).to.include.all.keys('id', 'name', 'calories', 'timeToPrepare', 'servings', 'ingredients');
@@ -52,6 +80,7 @@ describe('api/v1/recipes/food_search GET', function () {
 
         expect(firstRecipe.calories).to.equal('400');
         expect(firstRecipe.name).to.equal('chicken parmesan');
+        expect(firstRecipe.ingredients).to.have.lengthOf(2);
 
         let firstIngredient = firstRecipe.ingredients[0];
         expect(firstIngredient).to.include.all.keys('id', 'text');
@@ -68,7 +97,7 @@ describe('api/v1/recipes/food_search GET', function () {
         .then(response => {
           expect(response.statusCode).to.equal(200);
 
-          expect(response.body).to.have.lengthOf(0);
+          expect(response.body).to.deep.equal([]);
 
           done();
         })
