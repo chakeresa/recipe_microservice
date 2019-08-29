6bb9966514fa7411c145fc9a8953159cf71808e7
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-29 09:42:59 -0600

    Remove Heroku db setup route

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// Heroku postgres setup:
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-/* GET heroku db setup test. */
-router.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null };
-    res.send(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
 
 module.exports = router;
